9e299f6e4fb3fad76702db787cdf0a25e8e82ba4
FredCar <carricart.f@gmail.com>
2020-05-08 21:19:16 +0200

    Ajout de la Lemmatisation

--- execution.py
+++ execution.py
@@ -2,7 +2,9 @@ from pack.scrapeur import Scrapeur
 from pack.traitement import Traitement
 from pack.enregistrement import Enregistrement
 import pprint
+import time
 
+debut = time.time()
 
 # Instanciation des Classes
 scrap = Scrapeur()
@@ -12,7 +14,21 @@ enreg = Enregistrement()
 # Scraping
 sortie = scrap.scrap()
 
+# Traitement par articles
+cpt = 0
+total = 0
 for x in sortie.keys():
+    total += 1
+    # Contrôle des doublons avant traitement
+    requete = enreg.read({"titre": sortie[x]["titre"],
+                          "auteur": sortie[x]["auteur"]})
+
+    if len(list(requete)) > 0:
+        print(">>> Existe déjà !")
+        print(sortie[x]["titre"])
+        print("---------------------------------------")
+        continue
+
     # Concaténation du texte à traiter pour la matrice de termes
     chaine = ""
     chaine += sortie[x]["titre"] + " "
@@ -22,13 +38,27 @@ for x in sortie.keys():
         chaine += sortie[x]["extrait"] + " "
         chaine += sortie[x]["texte"]
 
+    # Conversion en liste de phrases (format attendu pour la création de la matrice de termes)
     parts = chaine.split(". ")
 
     # Nettoyage du texte
     liste_parts = []
     for part in parts:
+
+        # Lemmatisation
+        part = trait.lemmatisation(part)
+
+        # Nettoyage
         part = trait.nettoyage(part)
+
+        # TODO Stematisaton utile ou pas ?
+        # print(trait.stematisation(part))
+        # print("----------------------------------------------------")
+        # print(part)
+        # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
         liste_parts.append(part)
+
 
     # Matrice de termes
     mat = trait.matrice(liste_parts)
@@ -36,21 +66,16 @@ for x in sortie.keys():
 
     sortie[x]["matrice"] = mat
 
-    # Contrôle des doublons avant enregistrement
-    requete = enreg.read({"titre": sortie[x]["titre"],
-                          "auteur": sortie[x]["auteur"]})
 
     # Enregistrement
-    if len(list(requete)) > 0:
-        print(">>> Existe déjà !")
-        print(sortie[x]["titre"])
-        print("---------------------------------------")
-    else:
-        enreg.insert(sortie[x], method="one")
-        print("============Enregistré=================")
-        pprint.pprint(sortie[x])
-        print("=======================================")
+    cpt += 1
+    enreg.insert(sortie[x], method="one")
+    print("============Enregistré=================")
+    print(sortie[x]["titre"])
+    print("=======================================")
 
 
+temps = round(time.time() - debut, 2)
+print("Nb d'enregistrement :", cpt, "/", total, "articles, en :", temps, "secondes")
 #pprint.pprint(sortie)
 
--- pack/traitement.py
+++ pack/traitement.py
@@ -2,8 +2,14 @@ import re
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from stop_words import get_stop_words
+import nltk
+import spacy # Lemmatiseur
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+
+
+# nltk.download("punkt") # A charger la prmière fois
+# nltk.download("wordnet") # A charger la prmière fois
 
 
 class Traitement:
@@ -28,11 +34,63 @@ class Traitement:
         return x
 
 
-    def lematisation(self):
+    def tokenisation(self, phrase):
+        """
+        Converti les phrases en liste de mots
+        """
+        return nltk.word_tokenize(phrase)
+
+
+    def stematisation(self,phrase):
+        """
+        Supprime les suffixes
+        """
+        mots = self.tokenisation(phrase)
+        stem = nltk.stem.snowball.FrenchStemmer()
+        liste_de_mots = []
+        for mot in mots:
+            liste_de_mots.append(stem.stem(mot))
+
+        return liste_de_mots
+
+
+    def lemmatisation(self, phrase):
+        """
+        Renvoi la racine des mots
+        """
+        nlp = spacy.load('fr_core_news_md')
+
+        doc = nlp(phrase)
+        sortie = []
+        for token in doc:
+            sortie.append(token.lemma_)
+            # print(token, token.lemma_)
+        sortie = " ".join(sortie)
+
+        return sortie
+
+
+    # TODO Calcul de la positivité
+    def positivite(self):
+        """
+        Calcule la positivité de l'article
+        """
+        pass
+
+
+    # TODO Calcul de l'objectivité
+    def objectivite(self):
+        """
+        Calcule l'objectivité de l'article
+        """
         pass
 
 
     def matrice(self, x):
+        """
+        Renvoi la liste des termes et leur nombre d'occurences trié par ordre descendant
+        """
+        # TODO question à se poser : Matrice TFIDF ou pas ?
         cv = CountVectorizer(stop_words=self.french_stop_words)
         x_cv = cv.fit_transform(x)
 
@@ -47,4 +105,5 @@ class Traitement:
 
 # Test
 if __name__ == "__main__":
-    pass
+    trait = Traitement()
+    trait.spac()
