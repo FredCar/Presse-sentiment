6ab1ead37614a7384b94d31bb408a97558835702
FredCar <carricart.f@gmail.com>
2020-05-18 15:05:18 +0200

    Ajout d'un système de tri

--- web_app/app.py
+++ web_app/app.py
@@ -1,20 +1,19 @@
 from flask import Flask, render_template, request
 import time
 import datetime
-
+import random
 from pymongo import MongoClient
 import pandas as pd
 import matplotlib.pyplot as plt
 import base64
-
 import re
 from stop_words import get_stop_words
 from wordcloud import WordCloud
 
-# Pour pouvoir importer mes propres packages, même depuis un dossier parent
-import sys
-sys.path.insert(0, "/home/fred/Formation/Simplon/Exercisses/Projet_Mai2020/Projet_final")
-from pack.traitement import concatenation, nettoyage
+# # Pour pouvoir importer mes propres packages, même depuis un dossier parent
+# import sys
+# sys.path.insert(0, "/home/fred/Formation/Simplon/Exercisses/Projet_Mai2020/Projet_final")
+# from pack.traitement import concatenation, nettoyage
 
 
 # Connexion à la base de données
@@ -40,22 +39,6 @@ def read(collec=collec, filtre={}):
     return df
 
 
-def graph_pos_jour():
-    """
-    Enregistre un graph de la positivite par jours
-    """
-    df = read()
-
-    # df = df[df["positivite"] != 0]
-    df = df[df["date"] != "--"]
-
-    table = df.pivot_table(index=["date"], values="positivite")
-
-    titre = "Positivité moyenne des articles de presse par jours"
-    filename = "graph-jour"
-    ploteur(table.index, table.positivite, titre, filename)
-
-
 def date_heure(x):
     """
     retourne la date et l'heure concaténés
@@ -75,28 +58,46 @@ def hh(x):
     return liste
 
 
-def limiteur(limite=""):
+def limiteur(periode=30):
+    """Pour filtrer par période"""
     df = read()
-
     df = df[df["date"] != "--"]
     df = df[df["heure"] != "--"]
 
+    sec_par_jour = 60 * 60 * 24
+    limit = time.time() - (sec_par_jour * periode)
+    limit = datetime.datetime.fromtimestamp(limit)
+    limit = limit.strftime('%Y-%m-%d-%H')
+
     df["date-heure"] = df.apply(date_heure, axis=1)
-
-    hier = datetime.datetime.fromtimestamp(time.time() - (60 * 60 * 24))  # date d'hier
-    hier = hier.strftime('%Y-%m-%d-%H')
-
     df["date-heure"] = pd.to_datetime(df["date-heure"])
-    df = df[df["date-heure"] > hier]
+    df = df[df["date-heure"] > limit]
 
     return df
+
+
+def graph_pos_jour():
+    """
+    Enregistre un graph de la positivite par jours
+    """
+    df = read()
+
+    # df = df[df["positivite"] != 0]
+    df = df[df["date"] != "--"]
+
+    table = df.pivot_table(index=["date"], values="positivite")
+
+    titre = "Positivité moyenne des articles de presse par jours"
+    filename = "graph-jour"
+    ploteur(table.index, table.positivite, titre, filename)
+
 
 
 def graph_pos_24_heure():
     """
     Enregistre ung raph de la positivite pour les dernières 24 heures
     """
-    df = limiteur()
+    df = limiteur(periode=1)
 
     table = df.pivot_table(index=["date-heure"], values="positivite")
     table = table[1:]
@@ -125,25 +126,64 @@ def ploteur(x,y, titre="", filename="graph"):
     plt.savefig(chemin, format="png")
 
 
-def trieur(ascendant=False):
+def trieur(periode=30, ascendant=False):
     """
     Renvois les données du DataFrame en dico
     """
-    df = limiteur()
-
+    df = limiteur(periode)
     df.sort_values(by=["positivite"], axis=0, inplace=True, ascending=ascendant)
-
     data = df.T.to_dict()
+
+    for x in data:
+        pos = data[x]["positivite"]
+        if pos >= 0.8:
+            data[x]["couleur"] = "vert-5"
+        elif pos >= 0.6:
+            data[x]["couleur"] = "vert-4"
+        elif pos >= 0.4:
+            data[x]["couleur"] = "vert-3"
+        elif pos >= 0.2:
+            data[x]["couleur"] = "vert-2"
+        elif pos > 0:
+            data[x]["couleur"] = "vert-1"
+        elif pos == 0:
+            data[x]["couleur"] = "gris"
+        elif pos >= -0.2:
+            data[x]["couleur"] = "rouge-1"
+        elif pos >= -0.4:
+            data[x]["couleur"] = "rouge-2"
+        elif pos >= -0.6:
+            data[x]["couleur"] = "rouge-3"
+        elif pos >= -0.8:
+            data[x]["couleur"] = "rouge-4"
+        else:
+            data[x]["couleur"] = "rouge-5"
 
     return data
 
 
-def cloudeur(filename="cloud"):
-    french_stop_words = get_stop_words('french')
-    df = limiteur()
+def texteur(x):
+    liste = []
+    for y, z in x.items():
+        for t in range(z):
+            liste.append(y)
+    random.shuffle(liste)
+    chaine = " ".join(liste)
 
-    texte = df.apply(concatenation, axis=1)
-    titres_modif = list(map(nettoyage, texte))
+    return chaine
+
+
+def cloudeur(periode=1, positif=True, filename="cloud"):
+    french_stop_words = get_stop_words('french')
+    ajout_stop_words = ["etre", "plus", "meme", "ca", "tre", "tres", "dont", "apre", "apres", "selon", "celui"]
+    french_stop_words += ajout_stop_words
+
+    df = limiteur(periode)
+
+    # Travailler à partir de la matrice de termes
+    df["chaine_matrice"] = df["matrice"].apply(texteur)
+    texte = df["chaine_matrice"].values
+    titres_modif = list(texte)
     text = " ".join(titres_modif)
 
     wordcloud = WordCloud(stopwords=french_stop_words,
@@ -167,6 +207,20 @@ def statistiques():
     return data
 
 
+choix_periode = [("24 heures", 1),
+                 ("48 heures", 2),
+                 ("une semaine", 7),
+                 ("deux semaine", 14),
+                 ("un mois", 30),
+                 ("deux mois", 60),
+                 ("trois mois", 90),
+                 ("six mois", 182),
+                 ("un an", 365),
+                 ("tout", 100000)]
+
+choix_ordre_pos = [("Les plus positifs en premier", 0),
+                   ("Les plus négatifs en premier", 1)]
+
 
 #==============================================
 # \\\\\\\\\\\\\\ ---Flask app--- //////////////
@@ -182,6 +236,8 @@ def home():
     return render_template("pages/index.html", data=data)
 
 
+
+
 @app.route("/graph/")
 def graph():
 
@@ -191,20 +247,45 @@ def graph():
     return render_template("pages/graph.html")
 
 
+
+
 @app.route("/classement")
 def classement():
+    data = trieur(periode=1)
 
-    # TODO Selection de date ou de période
-    data = trieur()
+    return render_template("pages/classement.html", data=data, choix_periode=choix_periode, choix_ordre_pos=choix_ordre_pos)
 
-    return render_template("pages/classement.html", data=data)
+
+@app.route("/classement", methods=["POST"])
+def classement_actif():
+    periode = int(request.form["periode"])
+    ascendant = int(request.form["asc"])
+    ascendant = bool(ascendant)
+    data = trieur(periode, ascendant)
+    data["periode"] = periode
+    data["ascendant"] = ascendant
+
+    return render_template("pages/classement.html", data=data, choix_periode=choix_periode, choix_ordre_pos=choix_ordre_pos)
 
 
 @app.route("/nuage")
 def nuage():
-    cloudeur()
+    data = {}
+    data["periode"] = ""
+    return render_template("pages/nuage.html", data=data, choix_periode=choix_periode)
 
-    return render_template("pages/nuage.html")
+
+@app.route("/nuage", methods=["POST"])
+def nuage_actif():
+    data = {}
+    periode = int(request.form["periode"])
+    data["periode"] = periode
+
+    cloudeur(periode)
+
+    return render_template("pages/nuage.html", data=data, choix_periode=choix_periode)
+
+
 
 
 @app.route("/statistiques")
