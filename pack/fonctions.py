77bbf3226ae7dabc8c1d33bb916d22f06b707d9f
FredCar <carricart.f@gmail.com>
2020-05-20 22:17:19 +0200

    Ajout des articles similaires et réorganisation des fonctions de l'app web

---
+++ pack/fonctions.py
@@ -0,0 +1,263 @@
+import time
+import datetime
+import random
+from pymongo import MongoClient
+import pandas as pd
+import matplotlib.pyplot as plt
+from stop_words import get_stop_words
+from wordcloud import WordCloud
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+
+#==============================================
+# \\\\\\\\\\\\\\ ---Fonctions--- //////////////
+#==============================================
+def connection():
+    """ Connexion à la base de données  """
+    client = MongoClient("localhost", 27017)
+    dbase = client["presse-sentiment"]
+    collec = dbase["corpus"]
+
+    return collec
+
+
+def read(filtre={}):
+    """
+    Récupère les données de la base et les mets en DataFrame
+    """
+    collec = connection()
+
+    requete = collec.find(filtre)
+
+    df = pd.DataFrame(list(requete))
+
+    df.sort_values(by=["date"], axis=0, inplace=True)
+    # df = df.reset_index()
+
+    return df
+
+
+def date_heure(x):
+    """
+    retourne la date et l'heure concaténés
+    """
+    datetime = str(x["date"]) + "-" + str(x["heure"][:2])
+    return datetime
+
+
+def hh(x):
+    """
+    Prends la date-heure-min-sec
+    retourne juste l'heure
+    """
+    liste = []
+    for y in x:
+        liste.append(str(y)[11:13])
+    return liste
+
+
+def limiteur(periode=30):
+    """Pour filtrer par période"""
+    df = read()
+    df = df[df["date"] != "--"]
+    df = df[df["heure"] != "--"]
+
+    sec_par_jour = 60 * 60 * 24
+    limit = time.time() - (sec_par_jour * periode)
+    limit = datetime.datetime.fromtimestamp(limit)
+    limit = limit.strftime('%Y-%m-%d-%H')
+
+    df["date-heure"] = df.apply(date_heure, axis=1)
+    df["date-heure"] = pd.to_datetime(df["date-heure"])
+    df = df[df["date-heure"] > limit]
+
+    return df
+
+
+
+
+
+def graph_pos_jour():
+    """
+    Enregistre un graph de la positivite par jours
+    """
+    df = read()
+
+    # df = df[df["positivite"] != 0]
+    df = df[df["date"] != "--"]
+
+    table = df.pivot_table(index=["date"], values="positivite")
+
+    titre = "Positivité moyenne des articles de presse par jours"
+    filename = "graph-jour"
+    ploteur(table.index, table.positivite, titre, filename)
+
+
+
+def graph_pos_24_heure():
+    """
+    Enregistre ung raph de la positivite pour les dernières 24 heures
+    """
+    df = limiteur(periode=1)
+
+    table = df.pivot_table(index=["date-heure"], values="positivite")
+    table = table[1:]
+
+    a = table.index.values
+    table["hh"] = hh(a)
+
+    titre = "Positivité moyenne des articles de presse des dernières 24 heures"
+    filename = "graph-24-heure"
+    ploteur(table.hh, table.positivite, titre, filename)
+
+
+def ploteur(x,y, titre="", filename="graph"):
+    """
+    Crée et enregistre les graphs
+    """
+    plt.figure(figsize=(18, 8))
+    plt.plot(x, y, label="Coubre de positivite")
+    plt.axhline(y=0, linestyle="--", c="red", label="Neutre")
+    plt.title(titre, fontsize=28)
+    plt.xticks(rotation=70, fontsize=15)
+    plt.yticks(fontsize=15)
+    plt.legend(fontsize=15)
+
+    chemin = "static/images/" + filename
+    plt.savefig(chemin, format="png")
+
+
+def couleur_positivite(data):
+    for x in data:
+        data[x]["positivite"] = round((data[x]["positivite"]*100))
+        pos = data[x]["positivite"]
+        if pos >= 80:
+            data[x]["couleur"] = "vert-5"
+        elif pos >= 60:
+            data[x]["couleur"] = "vert-4"
+        elif pos >= 40:
+            data[x]["couleur"] = "vert-3"
+        elif pos >= 20:
+            data[x]["couleur"] = "vert-2"
+        elif pos > 0:
+            data[x]["couleur"] = "vert-1"
+        elif pos == 0:
+            data[x]["couleur"] = "gris"
+        elif pos >= -20:
+            data[x]["couleur"] = "rouge-1"
+        elif pos >= -40:
+            data[x]["couleur"] = "rouge-2"
+        elif pos >= -60:
+            data[x]["couleur"] = "rouge-3"
+        elif pos >= -80:
+            data[x]["couleur"] = "rouge-4"
+        else:
+            data[x]["couleur"] = "rouge-5"
+
+    return data
+
+def trieur(periode=30, ascendant=False):
+    """
+    Renvois les données du DataFrame en dico
+    """
+    df = limiteur(periode)
+    df.sort_values(by=["positivite"], axis=0, inplace=True, ascending=ascendant)
+    data = df.T.to_dict()
+    data = couleur_positivite(data)
+
+    return data
+
+
+def texteur(x):
+    liste = []
+    for y, z in x.items():
+        for t in range(z):
+            liste.append(y)
+    random.shuffle(liste)
+    chaine = " ".join(liste)
+
+    return chaine
+
+
+def stop_wordeur():
+    french_stop_words = get_stop_words('french')
+    ajout_stop_words = ["etre", "plus", "meme", "ca", "tre", "tres", "dont", "apre", "apres", "selon", "celui"]
+    french_stop_words += ajout_stop_words
+
+    return french_stop_words
+
+
+def cloudeur(periode=1, positif=True, filename="cloud"):
+    french_stop_words = stop_wordeur()
+
+    df = limiteur(periode)
+
+    # Travailler à partir de la matrice de termes
+    df["chaine_matrice"] = df["matrice"].apply(texteur)
+    texte = df["chaine_matrice"].values
+    titres_modif = list(texte)
+    text = " ".join(titres_modif)
+
+    wordcloud = WordCloud(stopwords=french_stop_words,
+                          background_color="white")
+    wordcloud.generate(text)
+
+    plt.figure(figsize=(18, 8))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+
+    chemin = "static/images/" + filename
+    plt.savefig(chemin, format="png")
+
+
+def statistiques():
+    df = read()
+    data = {}
+    data["total"] = df.shape[0]
+    data["journaux"] = len(df["auteur"].unique())
+
+    return data
+
+# Similarité
+def similaires(text, corpus):
+    french_stop_words = stop_wordeur()
+
+    # Entrainement du modèle TF-IDF
+    tf_idf_chat = TfidfVectorizer(stop_words=french_stop_words)
+    tf_phrases = tf_idf_chat.fit_transform(corpus)  # Matrice du corpus
+
+    text = [text]
+    tf_text = tf_idf_chat.transform(text)  # Matrice de la question utilisateur
+    vals = cosine_similarity(tf_text, tf_phrases)  # Calcul de similarité
+
+    ids = np.where(vals[0] > 0.25)  # ids des meilleurs scores
+    ids = ids[0]  # On ne veut qu'une liste [.] pas [[.]]
+
+    dico_id = {}  # Création d'un dico pour faciliter le tri
+    for id in ids:
+        dico_id[id] = vals[0][id]
+
+    dico_id = sorted(dico_id.items(), key=lambda x: x[1], reverse=True)  # Liste de tuples triée par similarité : + -> -
+    # # Affichage pour contrôle
+    # for id, score in dico_id:
+    #     print(id, ":", score, "=", phrases[id])
+    #     print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
+    ids = []  # Liste des meilleurs ids triés
+    for x in dico_id:
+        ids.append(x[0])
+
+    # if len(ids) >= 3:  # Limite du nb de réponses
+    #     ids = ids[:3]
+
+    reponses = []
+    if ids:
+        reponses = ids
+        # for id in ids:
+        #     reponses.append(corpus[id])
+    else:
+        reponses.append("Désolé, je ne trouve pas d'articles similaires.")
+
+    return reponses
--- web_app/app.py
+++ web_app/app.py
@@ -1,212 +1,13 @@
 from flask import Flask, render_template, request
-import time
 import datetime
-import random
-from pymongo import MongoClient
-import pandas as pd
-import matplotlib.pyplot as plt
-import base64
-import re
-from stop_words import get_stop_words
-from wordcloud import WordCloud
 
-# # Pour pouvoir importer mes propres packages, même depuis un dossier parent
-# import sys
-# sys.path.insert(0, "/home/fred/Formation/Simplon/Exercisses/Projet_Mai2020/Projet_final")
-# from pack.traitement import concatenation, nettoyage
+# Pour pouvoir importer mes propres packages, même depuis un dossier parent
+import sys
+sys.path.insert(0, "/home/fred/Formation/Simplon/Exercisses/Projet_Mai2020/Projet_final")
+from pack.fonctions import *
 
 
-# Connexion à la base de données
-client = MongoClient("localhost", 27017)
-dbase  = client["presse-sentiment"]
-collec = dbase["corpus"]
-
-
-#==============================================
-# \\\\\\\\\\\\\\ ---Fonctions--- //////////////
-#==============================================
-def read(collec=collec, filtre={}):
-    """
-    Récupère les données de la base et les mets en DataFrame
-    """
-    requete = collec.find(filtre)
-
-    df = pd.DataFrame(list(requete))
-
-    df.sort_values(by=["date"], axis=0, inplace=True)
-    df = df.reset_index()
-
-    return df
-
-
-def date_heure(x):
-    """
-    retourne la date et l'heure concaténés
-    """
-    datetime = str(x["date"]) + "-" + str(x["heure"][:2])
-    return datetime
-
-
-def hh(x):
-    """
-    Prends la date-heure-min-sec
-    retourne juste l'heure
-    """
-    liste = []
-    for y in x:
-        liste.append(str(y)[11:13])
-    return liste
-
-
-def limiteur(periode=30):
-    """Pour filtrer par période"""
-    df = read()
-    df = df[df["date"] != "--"]
-    df = df[df["heure"] != "--"]
-
-    sec_par_jour = 60 * 60 * 24
-    limit = time.time() - (sec_par_jour * periode)
-    limit = datetime.datetime.fromtimestamp(limit)
-    limit = limit.strftime('%Y-%m-%d-%H')
-
-    df["date-heure"] = df.apply(date_heure, axis=1)
-    df["date-heure"] = pd.to_datetime(df["date-heure"])
-    df = df[df["date-heure"] > limit]
-
-    return df
-
-
-def graph_pos_jour():
-    """
-    Enregistre un graph de la positivite par jours
-    """
-    df = read()
-
-    # df = df[df["positivite"] != 0]
-    df = df[df["date"] != "--"]
-
-    table = df.pivot_table(index=["date"], values="positivite")
-
-    titre = "Positivité moyenne des articles de presse par jours"
-    filename = "graph-jour"
-    ploteur(table.index, table.positivite, titre, filename)
-
-
-
-def graph_pos_24_heure():
-    """
-    Enregistre ung raph de la positivite pour les dernières 24 heures
-    """
-    df = limiteur(periode=1)
-
-    table = df.pivot_table(index=["date-heure"], values="positivite")
-    table = table[1:]
-
-    a = table.index.values
-    table["hh"] = hh(a)
-
-    titre = "Positivité moyenne des articles de presse des dernières 24 heures"
-    filename = "graph-24-heure"
-    ploteur(table.hh, table.positivite, titre, filename)
-
-
-def ploteur(x,y, titre="", filename="graph"):
-    """
-    Crée et enregistre les graphs
-    """
-    plt.figure(figsize=(18, 8))
-    plt.plot(x, y, label="Coubre de positivite")
-    plt.axhline(y=0, linestyle="--", c="red", label="Neutre")
-    plt.title(titre, fontsize=28)
-    plt.xticks(rotation=70, fontsize=15)
-    plt.yticks(fontsize=15)
-    plt.legend(fontsize=15)
-
-    chemin = "static/images/" + filename
-    plt.savefig(chemin, format="png")
-
-
-def trieur(periode=30, ascendant=False):
-    """
-    Renvois les données du DataFrame en dico
-    """
-    df = limiteur(periode)
-    df.sort_values(by=["positivite"], axis=0, inplace=True, ascending=ascendant)
-    data = df.T.to_dict()
-
-    for x in data:
-        pos = data[x]["positivite"]
-        if pos >= 0.8:
-            data[x]["couleur"] = "vert-5"
-        elif pos >= 0.6:
-            data[x]["couleur"] = "vert-4"
-        elif pos >= 0.4:
-            data[x]["couleur"] = "vert-3"
-        elif pos >= 0.2:
-            data[x]["couleur"] = "vert-2"
-        elif pos > 0:
-            data[x]["couleur"] = "vert-1"
-        elif pos == 0:
-            data[x]["couleur"] = "gris"
-        elif pos >= -0.2:
-            data[x]["couleur"] = "rouge-1"
-        elif pos >= -0.4:
-            data[x]["couleur"] = "rouge-2"
-        elif pos >= -0.6:
-            data[x]["couleur"] = "rouge-3"
-        elif pos >= -0.8:
-            data[x]["couleur"] = "rouge-4"
-        else:
-            data[x]["couleur"] = "rouge-5"
-
-    return data
-
-
-def texteur(x):
-    liste = []
-    for y, z in x.items():
-        for t in range(z):
-            liste.append(y)
-    random.shuffle(liste)
-    chaine = " ".join(liste)
-
-    return chaine
-
-
-def cloudeur(periode=1, positif=True, filename="cloud"):
-    french_stop_words = get_stop_words('french')
-    ajout_stop_words = ["etre", "plus", "meme", "ca", "tre", "tres", "dont", "apre", "apres", "selon", "celui"]
-    french_stop_words += ajout_stop_words
-
-    df = limiteur(periode)
-
-    # Travailler à partir de la matrice de termes
-    df["chaine_matrice"] = df["matrice"].apply(texteur)
-    texte = df["chaine_matrice"].values
-    titres_modif = list(texte)
-    text = " ".join(titres_modif)
-
-    wordcloud = WordCloud(stopwords=french_stop_words,
-                          background_color="white")
-    wordcloud.generate(text)
-
-    plt.figure(figsize=(18, 8))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis("off")
-
-    chemin = "static/images/" + filename
-    plt.savefig(chemin, format="png")
-
-
-def statistiques():
-    df = read()
-    data = {}
-    data["total"] = df.shape[0]
-    data["journaux"] = len(df["auteur"].unique())
-
-    return data
-
-
+# Variables utilisées pour le tri
 choix_periode = [("24 heures", 1),
                  ("48 heures", 2),
                  ("une semaine", 7),
@@ -249,11 +50,19 @@ def graph():
 
 
 
+
+###########################################""
+
+
 @app.route("/classement")
 def classement():
     data = trieur(periode=1)
 
-    return render_template("pages/classement.html", data=data, choix_periode=choix_periode, choix_ordre_pos=choix_ordre_pos)
+
+    nb_pages = int(len(data)/100)
+
+    return render_template("pages/classement.html", data=data, choix_periode=choix_periode,
+                           choix_ordre_pos=choix_ordre_pos, nb_pages=nb_pages)
 
 
 @app.route("/classement", methods=["POST"])
@@ -261,11 +70,35 @@ def classement_actif():
     periode = int(request.form["periode"])
     ascendant = int(request.form["asc"])
     ascendant = bool(ascendant)
+
     data = trieur(periode, ascendant)
     data["periode"] = periode
     data["ascendant"] = ascendant
 
     return render_template("pages/classement.html", data=data, choix_periode=choix_periode, choix_ordre_pos=choix_ordre_pos)
+
+
+@app.route("/articles_similaires", methods=["GET"])
+def classement_similaire():
+    df = read()
+    df["chaine_matrice"] = df["matrice"].apply(texteur)
+    corpus = df["chaine_matrice"].values
+
+    retour = request.values["id"]
+    df["_id"] = df["_id"].astype(str)
+    texte = df[df._id == retour]
+    texte = str(texte["chaine_matrice"].values)
+
+    reponse = similaires(texte, corpus)[1:]
+    reponse = df.iloc[reponse].T.to_dict()
+    reponse = couleur_positivite(reponse)
+
+    # TODO Rediriger vers une page spécifique ??
+    return render_template("pages/classement.html", data=reponse, choix_periode=choix_periode, choix_ordre_pos=choix_ordre_pos)
+
+
+###############################################################"
+
 
 
 @app.route("/nuage")
